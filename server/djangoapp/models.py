8ac18c372f732bf8f1d0ce1694ad50b53dbef9bd
astaylor11413 <astaylor140@gmail.com>
2026-05-10 03:36:26 -0400

    Updated format and syntax errors for djangoapp files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
+
     name = models.CharField(max_length=100)
     description = models.TextField()
 
@@ -10,9 +11,10 @@ class CarMake(models.Model):
         return self.name
 
 class CarModel(models.Model):
+
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealerID = models.CharField(max_length=50, null=True, blank=True)
-    #dealerID = models.IntegerField()
+    # dealerID = models.IntegerField()
     name = models.CharField(max_length=30)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -20,12 +22,10 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2026,
-        validators = [
-            MaxValueValidator(2026),
-            MinValueValidator(2015)
-        ]
-    )
+    year = models.IntegerField(default=2026, validators = [
+                MaxValueValidator(2026),
+                MinValueValidator(2015)
+            ])
     
     def __str__(self):
         return self.name
